f3e373a27d1b91dd70081dda0251806436d021d8
alexfaerman <alex.faerman@gmail.com>
2023-05-17 12:05:19 -0400

    add Json

--- src/main/java/com/workwolf/docupload/resume/ResumeController.java
+++ src/main/java/com/workwolf/docupload/resume/ResumeController.java
@@ -1,13 +1,19 @@
 package com.workwolf.docupload.resume;
 
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController
 public class ResumeController {
     private final ResumeService resumeService;
 
@@ -15,7 +21,7 @@ public class ResumeController {
         this.resumeService = resumeService;
     }
 
-    @RequestMapping(value="/upload", method = RequestMethod.POST)
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public String uploadResume(@RequestParam("file") MultipartFile file, Model model) {
         try {
             List<Education> education = resumeService.getEducation(file);
@@ -25,10 +31,26 @@ public class ResumeController {
         } catch (IOException e) {
             // error reading the file
             model.addAttribute("message", "Error uploading resume or reading the file.");
-       }
+        }
 
-       return "doneResume";
+        return "doneResume";
     }
+
+    @GetMapping(value = "/get-json")
+    public Map<String, Object> get(@RequestParam("file") MultipartFile file) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            List<Education> education = resumeService.getEducation(file);
+            List<Experience> pastExperience = resumeService.getPastExperience(file);
+            map.put("education", education);
+            map.put("pastExperience", pastExperience);
+        } catch (IOException e) {
+            // error reading the file
+            map.put("message", "Error uploading resume or reading the file.");
+        }
+        return map;
+    }
+
 
     @RequestMapping("/resume")
     public String uploadResume(){
